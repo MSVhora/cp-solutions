f358354e9cd507a3379bd64f55203afd10499060
Murtaza S Vhora <Murtazavhora@gmail.com>
2023-05-08 13:56:30 +0530

    Added Solution Author Component

---
+++ src/components/SolutionAuthor/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import styles from "./styles.module.css"
+
+export default function SolutionAuthor({ name }): JSX.Element {
+	return (
+		<div className={styles.wrapper}>
+			<span>Written by {name}</span>
+		</div>
+	)
+}
---
+++ src/theme/MDXComponents.ts
@@ -0,0 +1,15 @@
+import React from "react"
+// Import the original mapper
+import MDXComponents from "@theme-original/MDXComponents"
+import Tabs from "@theme/Tabs"
+import TabItem from "@theme/TabItem"
+import SolutionAuthor from "../components/SolutionAuthor"
+
+export default {
+	// Re-use the default mapping
+	...MDXComponents,
+	// custom
+	SolutionAuthor,
+	Tabs,
+	TabItem,
+}
